e1d514943fe7e4466409c0e4879c4d56442e34a0
Patriot Kusuma Sejati <patriot.kusumasejati@gmail.com>
2020-06-22 01:18:01 +0700

    Update Lagi dong

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-
-})
+  // Owl Carousel
+  $(".owl-carousel").owlCarousel({
+    dots: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
