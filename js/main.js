f4a9ae4ced851c6f1cd2ca9c663c30163d8ac151
Patriot Kusuma Sejati <patriot.kusumasejati@gmail.com>
2020-06-23 00:15:13 +0700

    Update Api

--- js/main.js
+++ js/main.js
@@ -1,18 +1,53 @@
+const api = {
+  urlNews: "http://newsapi.org/v2/",
+  keyNews: "b422121f5eec42f786f7420a95272b7e"
+}
+
+function topNewsId() {
+  $.ajax({
+    url: api.urlNews + "top-headlines?country=id&apiKey=" + api.keyNews,
+    success: function (res) {
+      // console.log(JSON.stringify(res));
+
+      if (res.status = "ok") {
+
+        let w = res.articles;
+
+        console.log()
+        $.each(w, function (i, data) {
+          $('#berita-indonesia').append(`
+            <div class="col-md-4 mt-5 ">
+              <div class="card">
+                <img class="card-img-top"
+                  src="` + data.urlToImage + `"
+                  alt="Card image cap" />
+                <div class="card-body">
+                  <a class="card-title h5" target="_blank" href="` + data.url + `">` + data.title + `</a>
+                  <p class="card-text">
+                    ` + data.content.substring(60) + `...
+                  </p>
+                  <p class="card-text">
+                      <small class="text-muted mr-2">
+                      <i class="far fa-calendar-check"></i>  
+                      ` + data.publishedAt.substring(0, 10) + `
+                      </small>
+                      <small class="text-muted">
+                      <i class="far fa-clock"></i>
+                      ` + data.publishedAt.substring(11, 16) + `</small>
+
+                  </p>
+                </div>
+              </div>
+            </div>
+          `);
+        });
+
+      }
+      // if ()
+    }
+  })
+}
+
 $(document).ready(function () {
-  // Owl Carousel
-  $(".owl-carousel").owlCarousel({
-    dots: true,
-    center: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 3,
-      },
-      1000: {
-        items: 5,
-      },
-    },
-  });
+  topNewsId();
 });
