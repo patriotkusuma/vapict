2025d478c511ae1e9aa6895d8a3f35ba34fa6311
Patriot Kusuma Sejati <patriot.kusumasejati@gmail.com>
2020-07-09 21:37:10 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -97,6 +97,99 @@ function timeDateFormat(val) {
   return td;
 }
 
+function hajjNews() {
+  $.ajax({
+    url: api.urlNews + "everything?q=hajj&apiKey=" + api.keyNews1,
+    success: function (res) {
+      if ((res.status = "ok")) {
+        let w = res.articles;
+
+        $("#top-news-hajj-3").append(
+          `
+          <div class="col-sm-8 my-auto">
+            <div class="card shadow-sm">
+                  
+                  <img class="w-100" src="` +
+            w[w.length - 3].urlToImage +
+            `" />
+                  <div class="card-img-overlay d-flex overlay-dark">
+                      <div class="align-self-center mx-auto text-light">
+                          <a href="` +
+            w[w.length - 3].url +
+            `" target="_blank" class="h2 font-weight-bolder text-light">` +
+            w[w.length - 3].title +
+            `
+                              </a>
+                          <br class="mb-3" />
+                          <a class="mt-5">
+                              <i class="fas fa-user mr-2"></i> <span >` +
+            w[w.length - 3].author +
+            `</span> -
+                              <span ><i class="far fa-clock mr-2"></i>` +
+            timeDateFormat(w[w.length - 3].publishedAt).dt +
+            `</span>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-sm-4 my-auto">
+              <div class="card mt-2 shadow-sm">
+                  <img class="w-100" src="` +
+            w[w.length - 2].urlToImage +
+            `" />
+                  <div class="card-img-overlay d-flex overlay-dark">
+                      <div class="align-self-center my-auto mx-auto text-light">
+                          <a href="` +
+            w[w.length - 2].url +
+            `" target="_blank" class="h5 font-weight-bolder text-light"
+                              id="text-title-corona">` +
+            w[w.length - 2].title +
+            `</a>
+                          <br />
+                          <a class="mt-3">
+                              <i class="fas fa-user mr-2"></i> <span>` +
+            w[w.length - 2].author +
+            `</span> -
+                              <span id="time-edit-corona-news"><i class="far fa-clock mr-2"></i>` +
+            timeDateFormat(w[w.length - 2].publishedAt).dt +
+            `</span>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+              <div class="card mt-2 shadow-sm">
+                  <img class="w-100" src="` +
+            w[w.length - 1].urlToImage +
+            `" />
+                  <div class="card-img-overlay d-flex overlay-dark">
+                      <div class="align-self-center mx-auto text-light">
+                          <a href="` +
+            w[w.length - 1].url +
+            `" target="_blank" class="h5 font-weight-bolder text-light"
+                              id="text-title-corona">` +
+            w[w.length - 1].title +
+            `</a>
+                          <br />
+                          <a class="mt-3">
+                              <i class="fas fa-user mr-2"></i> <span id="author-corona">` +
+            w[w.length - 1].author +
+            `</span> -
+                              <span id="time-edit-corona-news"><i class="far fa-clock mr-2"></i>` +
+            timeDateFormat(w[w.length - 1].publishedAt).dt +
+            `</span>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          `
+        );
+      }
+    },
+  });
+}
+
 // Function Top News Indonesia
 function topNewsId() {
   $.ajax({
@@ -1181,6 +1274,7 @@ $(document).ready(function () {
   } else if (pathname == "/religi.html") {
     daftarKota();
     islamicNews();
+    hajjNews();
 
     ipInfo();
     $("#btn-search-jadwal").click(function () {
